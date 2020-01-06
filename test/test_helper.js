607ec4e970fe94522b5eef2778d911d13e54fc71
“Young <hogusong1@gmail.com>
2020-01-06 11:43:28 -0500

    update 'test_helper': add a callback function.

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,7 +5,20 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log("Good to go!"))
-  .on('error', (err) => console.warn('Warning', err));  
+before(done => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => {
+      console.log("Good to go!");
+      done();
+    })
+    .on('error', (err) => console.warn('Warning', err));    
+});
+
+beforeEach((done) => {
+  // 'done()' is a callback function. It notifies all the test parts when this process is done.
+  const { users } = mongoose.connection.collections;
+  if (users) {
+    users.drop(() => done());
+  } else done();
+});
